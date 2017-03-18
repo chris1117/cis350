5da15052ece102d806c3359ec81463300a517d61
Christian <cyoung1117@gmail.com>
2017-03-18 09:34:16 -0400

    Complete program description

--- mstFlow/Source.cpp
+++ mstFlow/Source.cpp
@@ -1,6 +1,14 @@
 /*
 	Name        : Eranus Thompson
-	Description : 
+	Description : Given a gaph that has a complete set of pipes (edges) that connect
+			      all junctions (vertices), the program uses minimum spanning tree, 
+				  find, or union to pick the pipes (edges) which most closely have the 
+				  same capacity to be replaced. The input (read from a keyboard) consists 
+				  of multiple datasets followed by a line containing two zeros separated
+				  by a space (which indicates the end of the input datasets). The junctions
+				  (vertices) >= 2 and the pipes (edges) <= 0 && >= ( vertices(vertices - 1)/2).
+				  For a complete set of pipes that connect all junctions, the program outputs
+				  the set with the smallest range of capacities. Otherwise, it outputs -1.				  
 */
 
 #include <iostream>
